6586fc873176cd4db14a98002cdbf31b386cf236
LinailnoxNotFound <81369236+Linailnox@users.noreply.github.com>
2024-05-22 21:24:47 +0800

    Update patch.bat

--- patch.bat
+++ patch.bat
@@ -9,19 +9,19 @@ if not exist idea_logo.png (
         jar xf app.jar idea_logo.png
 	echo 完成！
     ) else (
-        echo 文件已存在，未提取！
+        echo 文件已存在！
     )
 echo 正在提取idea_logo@2x.png！
 if not exist idea_logo@2x.png (
         jar xf app.jar idea_logo@2x.png
 	echo 完成！
     ) else (
-        echo 文件已存在，未提取！
+        echo 文件已存在！
     )
 echo 请替换目录下的idea_logo.png和idea_logo@2x.png
 timeout /T -1
 echo 正在替换！
 jar -uvf app.jar idea_logo.png
 jar -uvf app.jar idea_logo@2x.png
-echo 替换完成
+echo 替换完成！
 pause
